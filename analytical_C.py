498b885a1aa4b4b299b85d047d4fb79b0796dd2b
Antoine <antoine.mouchamps@gmail.com>
2026-05-05 00:21:44 +0200

    Simplify analytical C

--- analytical_C.py
+++ analytical_C.py
@@ -21,35 +21,21 @@ if __name__ == "__main__":
         - r_phase_cable_conductor
         - phase_cable_insulation_thickness
     )  # thickness of semiconductor layer
-    lead_sheath_thickness = 2.4 * mm  # thickness of lead sheath
-    hdpe_sheath_thickness = 2.1 * mm  # thickness of inner sheath
-    r_phase_cable_outer = (
-        r_phase_cable_with_insulation + lead_sheath_thickness + hdpe_sheath_thickness
-    )  # diameter of phase conductor with insulation and sheaths
 
-    steel_wire_armour_thickness = 7 * mm  # thickness of Steel pipe
-    outer_sheath_cable = 4 * mm
-    r_cable_outer = (185 + 0) / 2 * mm  # cable outer diameter
+    epsilon_0 = 8.854187818e-12
+    epsilon_insu = epsilon_0 * 2.5
+    epsilon_semi = epsilon_0 * 20.0
 
-    epsilon_insu = 8.854187818e-12 * 2.25
     r_c = r_phase_cable_conductor
-    r_s_in = (
-        r_phase_cable_conductor
-        + semiconductor_thickness
-        + phase_cable_insulation_thickness
-    )
-    r_s_out = r_s_in + lead_sheath_thickness
-    R_a = r_cable_outer - steel_wire_armour_thickness - outer_sheath_cable
-
-    C_phase = coaxial_c(epsilon_insu, r_c, r_s_in)
-    d = 2 * r_phase_cable_outer / (np.sqrt(3))
-    r_eq = np.cbrt(3 * r_s_out * d**2)
-    C_armor = coaxial_c(epsilon_insu, r_eq, R_a)
+    r1 = r_c + semiconductor_thickness
+    r_s_in = r1 + phase_cable_insulation_thickness
+    C_semi = coaxial_c(epsilon_semi, r_c, r1)
+    C_insu = coaxial_c(epsilon_insu, r1, r_s_in)
+    print(f"C_semi = {C_semi:.2e} F/m")
+    print(f"C_insu = {C_insu:.2e} F/m")
+    C_phase = (C_semi * C_insu) / (C_semi + C_insu)
 
     print(f"C_phase = {C_phase:.2e} F/m")
-    print(f"C_armor = {C_armor:.2e} F/m")
 
-    C_tot_per_phase = (C_phase * (C_armor / 3)) / (C_phase + (C_armor / 3))
-    print(f"C_tot_per_phase = {C_tot_per_phase:.2e} F/m")
-    C_tot = 3 * C_tot_per_phase
+    C_tot = 3 * C_phase
     print(f"C_tot = {C_tot:.2e} F/m")
